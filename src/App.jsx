7ea5a69f2c2a9c79340a66ed77b605bf1910f4b0
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2024-01-15 00:49:56 +0100

    add bootstrap + header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header className='d-flex justify-content-between align-items-center'>
+      
+      <div className='d-flex align-items-center'>
+        <img className='ms-3 img-fluid w-25' src="/assets/img/yu-gi-oh-logo.png" alt="" />
+        <h1 className=' text-white'>YU-GI-OH API</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <select className='me-3 form-select w-25' name="archetypes" id="">
+        <option value="0">seleziona</option>
+        <option value="0">hero</option>
+        <option value="0">hero2</option>
+      </select>
+    </header>
+    <main></main>
     </>
   )
 }
